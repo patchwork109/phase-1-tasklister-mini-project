91e2ca487669c1a47d951538d58e832dbffb9fc4
Samantha Genevay <samanthagenevay@gmail.com>
2023-02-01 10:52:07 -0600

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const button = document.getElementById('button');
+const form = document.getElementById('create-task-form');
+const newListItem = document.getElementById('tasks');
+
+function addTaskToList(eventObj) {
+  eventObj.preventDefault();
+  const listItem = document.createElement('li');
+  const newText = eventObj.target['new-task-description'].value
+  listItem.textContent = newText
+  newListItem.append(listItem)
+}
+
+form.addEventListener('submit', addTaskToList);
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
